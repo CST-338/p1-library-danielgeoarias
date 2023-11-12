f3413dbe011e54990e7de4eccda7bc5d3ca226f7
Daniel Arias <danielgarias199@gmail.com>
2023-11-11 20:26:46 -0800

    Added addBook and some more methods

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -7,7 +7,7 @@ public class Library {
 
   private static final int LENDING_LIMIT = 5;
   private String name;
-  static int libraryCard;
+  private static int libraryCard;
   private List<Reader> readers;
   private HashMap<String, Shelf> shelves;
   private HashMap<Book, Integer> books;
@@ -26,4 +26,34 @@ public class Library {
     System.out.println("Not implemented");
     return Code.NOT_IMPLEMENTED_ERROR;
   }
+
+  public Code addBook(Book newBook){
+    if(this.books.containsKey(newBook)){
+      Integer count = this.books.get(newBook);
+      count++;
+      System.out.println(count + " copies of " + newBook.getTitle() + " in the stacks");
+    }
+    if (!this.books.containsKey(newBook)) {
+      this.books.put(newBook, 1);
+      System.out.println(newBook.getTitle() + " added to the stacks");
+    }
+    if(this.shelves.containsKey(newBook.getSubject())) {
+      this.shelves.
+      return Code.SUCCESS;
+    }
+    System.out.println("No shelf for " + newBook.getSubject() + " books");
+    return Code.SHELF_EXISTS_ERROR;
+
+  }
+  public returnBook(Reader reader, Book book){
+    if(!reader.hasBook(book)) {
+      System.out.println(reader.getName() + " doesn't have " book.getTitle() + " checked out");
+      return Code.READER_DOESNT_HAVE_BOOK_ERROR;;
+    }
+    //if()
+  }
+  private Code addBookToShelf(Book book, Shelf shelf){
+
+  }
+
 }
